6ebe5f8ffd608803d1a528aecfb2697973db6b54
pkassar <paulkassar@gmail.com>
2018-05-30 18:06:17 -0400

    renamed app

--- client/components/App.js
+++ client/components/App.js
@@ -1,99 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import fetch from 'isomorphic-fetch';
 import Playlist from './Playlist';
 import Albums from './Albums';
+import SetList from './SetList'
+
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            listOfItems: [],
-            value: '',
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.addToList = this.addToList.bind(this);
-        this.delete = this.delete.bind(this);
-        this.getUrl = this.getUrl.bind(this)
-    }
-
-    componentDidMount() {
-        fetch('/allSets', { method: 'GET' })
-        .then(res => { return res.json() })
-            .then(res => {
-                if (res) {
-                    this.setState({
-                        listOfItems: res,
-                        value: ''
-                    })
-                }
-            })
-    }
-
-    addToList(data) {
-        fetch('/add', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ description: data, date: new Date() }) })
-            .then((res) => {
-                if (res.status === 200) {
-                    this.setState({
-                        listOfItems: [...this.state.listOfItems, { description: data }],
-                        value: ''
-                    });
-                }
-            });
-    }
-
-    delete(item) {
-        fetch('/', { method: 'DELETE', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(item) })
-            .then((res) => {
-                if (res.status === 200) {
-                    const newState = [...this.state.listOfItems];
-                    if (newState.indexOf(item) > -1) {
-                        newState.splice(newState.indexOf(item), 1);
-                        this.setState({ listOfItems: newState, value: '' });
-                    }
-                }
-            });
-    }
-
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        this.addToList(this.getUrl(this.state.value));
-    }
-
-    getUrl(str){
-        return str.substring(88,str.length-11)
-    }
-
-    render() {
-        const listOfItems = this.state.listOfItems;
-        return (
-            <div>
-                <h2>Set Lists</h2>
-                <div className='set-list'>
-                    <ul>
-                        {listOfItems.map(i => <li key={i}>
-                            <iframe width="100%" height="110" scrolling="no" frameBorder="no" allow="autoplay" src={i.description}></iframe>
-                            <button key={i} onClick={this.delete.bind(this, i)}>Delete</button>
-                        </li>)}
-                    </ul>
-                </div>
-                <div>
-                    <form onSubmit={this.handleSubmit}>
-                        <input className='field' placeholder='Add to your set list' value={this.state.value} onChange={this.handleChange} />
-                        <input className='button' type='submit' value='Add' />
-                    </form>
-                </div>
-                <Playlist/>
-                <Albums/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id='components-wrapper'>
+        < SetList />
+        < Playlist />
+        <Albums />
+      </div>
+      )
+  }
 }
-
 
 
 export default App;
